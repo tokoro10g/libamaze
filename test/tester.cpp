2f2ad661a4e578114522a13cd98f96160963874f
Teng Tokoro <tokoro10g@tokor.org>
2019-08-17 18:53:01 +0900

    Updated tester

--- test/tester.cpp
+++ test/tester.cpp
@@ -8,25 +8,26 @@
 
 using namespace Amaze;
 
-/*
-void sense(Maze& maze, const Maze& reference_maze, Coordinates c, std::vector<Coordinates>& changed_coordinates)
+template <uint8_t W>
+void senseFourWay(Maze<W>& maze, const Maze<W>& reference_maze, Coordinates c, std::vector<Coordinates>& changed_coordinates)
 {
-    c.dir = North;
+    constexpr int8_t dx[4] = { 0, 1, 0, -1 };
+    constexpr int8_t dy[4] = { 1, 0, -1, 0 };
     for (int i = 0; i < 4; i++) {
-        c.dir.half = (1 << i);
-        if (maze.isCheckedWall(c)) {
+        Position p = { static_cast<uint8_t>(c.pos.x + dx[i]), static_cast<uint8_t>(c.pos.y + dy[i]) };
+        if (maze.isCheckedWall(p)) {
             continue;
         }
-        maze.setCheckedWall(c);
-        if (reference_maze.isSetWall(c)) {
-            maze.setWall(c);
-            changed_coordinates.push_back(c);
+        maze.setCheckedWall(p, true);
+        if (reference_maze.isSetWall(p)) {
+            maze.setWall(p, true);
+            Position pto = { static_cast<uint8_t>(p.x + dx[i]), static_cast<uint8_t>(p.y + dy[i]) };
+            changed_coordinates.push_back({ pto, NoDirection });
         } else {
-            maze.resetWall(c);
+            maze.setWall(p, false);
         }
     }
 }
-*/
 
 int main()
 {
@@ -50,18 +51,18 @@ int main()
     std::cout << std::endl;
 
     FourWayStepMapGraph<uint16_t> mg2(reference_maze);
-    auto e21 = mg2.getEdge({ 0, 1, North }, { 0, 1, East });
-    auto e22 = mg2.getEdge({ 0, 1, North }, { 0, 2, North });
-    auto e23 = mg2.getEdge({ 0, 1, East }, { 1, 2, North });
+    auto e21 = mg2.getEdge({ 0, 2, NoDirection }, { 0, 2, NoDirection });
+    auto e22 = mg2.getEdge({ 0, 0, NoDirection }, { 0, 2, NoDirection });
+    auto e23 = mg2.getEdge({ 0, 0, NoDirection }, { 2, 0, NoDirection });
     std::cout << (e21.first ? "true" : "false") << " " << (int)e21.second << std::endl;
     std::cout << (e22.first ? "true" : "false") << " " << (int)e22.second << std::endl;
     std::cout << (e23.first ? "true" : "false") << " " << (int)e23.second << std::endl;
     std::cout << std::endl;
 
     FourWayStepMapGraph<float> mg3(reference_maze);
-    auto e31 = mg3.getEdge({ 0, 1, North }, { 0, 1, East });
-    auto e32 = mg3.getEdge({ 0, 1, North }, { 0, 2, North });
-    auto e33 = mg3.getEdge({ 0, 1, East }, { 1, 2, North });
+    auto e31 = mg3.getEdge({ 0, 2, NoDirection }, { 0, 2, NoDirection });
+    auto e32 = mg3.getEdge({ 0, 0, NoDirection }, { 0, 2, NoDirection });
+    auto e33 = mg3.getEdge({ 0, 0, NoDirection }, { 2, 0, NoDirection });
     std::cout << (e31.first ? "true" : "false") << " " << e31.second << std::endl;
     std::cout << (e32.first ? "true" : "false") << " " << e32.second << std::endl;
     std::cout << (e33.first ? "true" : "false") << " " << e33.second << std::endl;
@@ -69,9 +70,9 @@ int main()
 
     SixWayWallNodeGraph mg4(reference_maze);
     auto solver4 = DStarLite(mg4);
-    auto e41 = mg4.getEdge({ 0, 1, North }, { 0, 1, East });
-    auto e42 = mg4.getEdge({ 0, 1, North }, { 0, 2, North });
-    auto e43 = mg4.getEdge({ 0, 1, East }, { 1, 1, North });
+    auto e41 = mg4.getEdge({ 0, 3, South }, { 1, 2, East });
+    auto e42 = mg4.getEdge({ 0, 3, North }, { 0, 5, North });
+    auto e43 = mg4.getEdge({ 1, 2, East }, { 2, 3, North });
     std::cout << (e41.first ? "true" : "false") << " " << (int)e41.second << std::endl;
     std::cout << (e42.first ? "true" : "false") << " " << (int)e42.second << std::endl;
     std::cout << (e43.first ? "true" : "false") << " " << (int)e43.second << std::endl;
@@ -89,17 +90,13 @@ int main()
     std::cout << "The goal is " << id_goal << std::endl;
     std::cout << std::endl;
 
-    int timeout_count = 0;
-    //while (solver1.getCurrentNodeId() != id_goal || timeout_count > 100) {
-    for (int i=0;i<100;i++) {
-        std::cout << mg1.coordByNodeId(solver1.getCurrentNodeId()) << std::endl;
+    while (solver1.getCurrentNodeId() != id_goal) {
+        std::cout << mg1.coordByNodeId(solver1.getCurrentNodeId()) << ": " << (int)solver1.getCurrentNodeId() << std::endl;
         solver1.preSense();
         solver1.postSense(std::vector<Coordinates>());
-        timeout_count++;
     }
     std::cout << id_goal << std::endl;
 
-    /*
     std::cout << std::endl;
     std::cout << "=======Testing D* Lite for 6-way Graph=======" << std::endl;
     std::cout << std::endl;
@@ -110,12 +107,10 @@ int main()
     std::cout << "The goal is " << id_goal4 << std::endl;
     std::cout << std::endl;
 
-    timeout_count = 0;
-    while (solver4.getCurrentNodeId() != id_goal4 || timeout_count > 100) {
-        std::cout << mg4.coordByNodeId(solver4.getCurrentNodeId()) << std::endl;
+    while (solver4.getCurrentNodeId() != id_goal4) {
+        std::cout << mg4.coordByNodeId(solver4.getCurrentNodeId()) << ": " << (int)solver4.getCurrentNodeId() << std::endl;
         solver4.preSense();
         solver4.postSense(std::vector<Coordinates>());
-        timeout_count++;
     }
     std::cout << id_goal4 << std::endl;
 
@@ -123,11 +118,11 @@ int main()
     std::cout << "=======Testing Dynamic Search Using D* Lite for 4-way Graph=======" << std::endl;
     std::cout << std::endl;
 
-    Maze maze(max_maze_width, max_maze_width);
-    Coordinates goal_coordinates = reference_maze.getGoal();
-    Utility::loadEmptyMaze(max_maze_width, max_maze_width, goal_coordinates.x, goal_coordinates.y, maze);
-    maze.setCheckedWall({ 0, 1 });
-    maze.setCheckedWall({ 1, 0 });
+    Maze<max_maze_width> maze;
+    Position goal_position = reference_maze.getGoal();
+    Utility::loadEmptyMaze(goal_position.x, goal_position.y, maze);
+    maze.setCheckedWall({ 0, 1 }, true);
+    maze.setCheckedWall({ 1, 0 }, true);
     FourWayStepMapGraph mg5(maze);
     auto solver5 = DStarLite(mg5);
 
@@ -149,7 +144,7 @@ int main()
 
         solver5.preSense();
         id = solver5.getCurrentNodeId();
-        sense(maze, reference_maze, mg5.coordByNodeId(id), changed_coordinates);
+        senseFourWay(maze, reference_maze, mg5.coordByNodeId(id), changed_coordinates);
         //std::cout << "After sense: " << std::endl;
         //cd = maze.getCell(id);
         //std::cout << id << " " << mg5.coordByNodeId(id) << ": " << std::bitset<8>(cd.byte).to_string() << std::endl;
@@ -161,6 +156,5 @@ int main()
         solver5.postSense(changed_coordinates);
     }
     std::cout << id_goal5 << std::endl;
-    */
     return 0;
 }
