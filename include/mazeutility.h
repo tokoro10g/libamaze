04d0825ac5055b0464899666c0a73545611cd04a
Teng Tokoro <tokoro10g@tokor.org>
2019-08-20 23:14:25 +0900

    Moved operator overloads for printing to the header

--- include/mazeutility.h
+++ include/mazeutility.h
@@ -139,7 +139,29 @@ namespace Utility {
     }
 }
 
-std::ostream& operator<<(std::ostream& os, Direction d);
-std::ostream& operator<<(std::ostream& os, Coordinates c);
+std::ostream& operator<<(std::ostream& os, Direction d)
+{
+    if (d.bits.NORTH) {
+        os << 'N';
+    }
+    if (d.bits.EAST) {
+        os << 'E';
+    }
+    if (d.bits.WEST) {
+        os << 'W';
+    }
+    if (d.bits.SOUTH) {
+        os << 'S';
+    }
+    if (d.half == 0) {
+        os << '0';
+    }
+    return os;
+}
+std::ostream& operator<<(std::ostream& os, Coordinates c)
+{
+    os << "(" << (int)c.pos.x << ", " << (int)c.pos.y << ", " << c.dir << ")";
+    return os;
+}
 
 }
