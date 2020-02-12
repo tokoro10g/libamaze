ec3e8864a8e1644d6fa8a052e0f0b11c5c3f520c
Teng Tokoro <tokoro10g@tokor.org>
2020-02-13 01:53:55 +0900

    Added a simple build script for ease of testing and coverage analysis

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd build
+make -j13
+make test
+cd test/CMakeFiles/unit_test.dir/
+lcov -d . -c -o coverage.info
+lcov -r coverage.info '*/googletest/*' '*/test/*' '*/c++/*' '/usr/*' -o coverageFiltered.info
+genhtml -o lcovHtml --num-spaces 4 -s --legend coverageFiltered.info
