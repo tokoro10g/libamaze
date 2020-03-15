8336183dbcf28995bf3e89bdb2c4a6549956cf0d
Teng Tokoro <tokoro10g@tokor.org>
2020-03-15 11:55:39 +0900

    [WIP] .travis.yml

---
+++ check.sh
@@ -0,0 +1,8 @@
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make -s -j13
+make test
+cd test/CMakeFiles/unit_test.dir/
+lcov -d . -c -o coverage.info
+lcov -r coverage.info '*/googletest/*' '*/test/*' '*/c++/*' '/usr/*' -o coverageFiltered.info
+genhtml -o lcovHtml --num-spaces 4 -s --legend coverageFiltered.info
